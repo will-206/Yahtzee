e522fedb92dd937475bc712deacdcfd524763c46
will-206 <will9_30@yahoo.com>
2016-12-08 14:40:20 -0800

    Fixed lock symbol spacing

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-"use strict";
+'use strict';
 // global vars //
-let gameStats = {
-  remainingRolls: 3
-}
+const gameStats = {
+  playerName: 'player 1',
+  remainingRolls: 3,
+  highScore: JSON.parse(localStorage.getItem('highScore')) || 1
+};
 
 const dice = [
   {currentValue: 0, locked: false},
@@ -13,23 +15,23 @@ const dice = [
 ];
 
 const scoreCard = {
-  aces: {name: "Aces", score: 0, used: false, section: "upper"},
-  twos: {name: "Twos", score: 0, used: false, section: "upper"},
-  threes: {name: "Threes", score: 0, used: false, section: "upper"},
-  fours: {name: "Fours", score: 0, used: false, section: "upper"},
-  fives: {name: "Fives", score: 0, used: false, section: "upper"},
-  sixes: {name: "Sixes", score: 0, used: false, section: "upper"},
-  bonus: {name: "Bonus", score: 0, used: false, section: "none"},
-  threeOfAKind: {name: "Three of a kind", score: 0, used: false, section: "lower"},
-  fourOfAKind: {name: "Four of a kind", score: 0, used: false, section: "lower"},
-  fullHouse: {name: "Full House", score: 0, used: false, section: "lower"},
-  smStraight: {name: "Small Straight", score: 0, used: false, section: "lower"},
-  lgStraight: {name: "Large Straight", score: 0, used: false, section: "lower"},
-  yahtzee: {name: "Yahtzee!", score: 0, used: false, section: "lower"},
-  // yahtzeeBonus: {name: "Yahtzee Bonus", score: 0, used: false,},
-  chance: {name: "Chance", score: 0, used: false, section: "lower"},
-  total: {name: "Total", score: 0, used: false, section: "none"}
-}
+  aces: {name: 'Aces', score: 0, used: false, section: 'upper'},
+  twos: {name: 'Twos', score: 0, used: false, section: 'upper'},
+  threes: {name: 'Threes', score: 0, used: false, section: 'upper'},
+  fours: {name: 'Fours', score: 0, used: false, section: 'upper'},
+  fives: {name: 'Fives', score: 0, used: false, section: 'upper'},
+  sixes: {name: 'Sixes', score: 0, used: false, section: 'upper'},
+  bonus: {name: 'Bonus', score: 0, used: false, section: 'none'},
+  threeOfAKind: {name: 'Three of a kind', score: 0, used: false, section: 'lower'},
+  fourOfAKind: {name: 'Four of a kind', score: 0, used: false, section: 'lower'},
+  fullHouse: {name: 'Full House', score: 0, used: false, section: 'lower'},
+  smStraight: {name: 'Small Straight', score: 0, used: false, section: 'lower'},
+  lgStraight: {name: 'Large Straight', score: 0, used: false, section: 'lower'},
+  yahtzee: {name: 'Yahtzee!', score: 0, used: false, section: 'lower'},
+  // yahtzeeBonus: {name: 'Yahtzee Bonus', score: 0, used: false,},
+  chance: {name: 'Chance', score: 0, used: false, section: 'lower'},
+  total: {name: 'Total', score: 0, used: false, section: 'none'}
+};
 
 // api call //
 const $xhr = $.ajax({
@@ -42,7 +44,7 @@ $xhr.done((data) => {
   if ($xhr.status !== 200) {
     return;
   }
-  gameStats.playerName = (data.results[0].login.username.replace(/\d/g, ""));
+  gameStats.playerName = (data.results[0].login.username.replace(/\d/g, ''));
 });
 
 $xhr.fail((err) => {
@@ -58,7 +60,7 @@ function getScore(category) {
     resultArr.push(dice[key].currentValue);
   }
 
-  let resultString = resultArr.sort().toString().replace(/,/g, "");
+  let resultString = resultArr.sort().toString().replace(/,/g, '');
 
   function addScore(number) {
     for(let elem in resultArr) {
@@ -77,95 +79,100 @@ function getScore(category) {
   }
 
   switch (category) {
-    case "aces":
+    case 'aces':
     addScore(1);
     break;
 
-    case "twos":
+    case 'twos':
     addScore(2);
     break;
 
-    case "threes":
+    case 'threes':
     addScore(3);
     break;
 
-    case "fours":
+    case 'fours':
     addScore(4);
     break;
 
-    case "fives":
+    case 'fives':
     addScore(5);
     break;
 
-    case "sixes":
+    case 'sixes':
     addScore(6);
     break;
 
-    case "bonus":
+    case 'bonus':
     let subTotalUpper = 0;
     for (let elem in scoreCard) {
-      if (scoreCard[elem].used === true && scoreCard[elem].section === "upper") {
+      if (scoreCard[elem].used === true && scoreCard[elem].section === 'upper') {
         subTotalUpper += scoreCard[elem].score;
       }
     }
-    console.log("subTotal = " + subTotalUpper);
+
     if (subTotalUpper >= 63) {
       score = 35;
       scoreCard[category].used = true;
     }
     break;
 
-    case "threeOfAKind":
+    case 'threeOfAKind':
     if (/(.)\1{2}/.test(resultString)) {
       score = total();
     }
     break;
 
-    case "fourOfAKind":
+    case 'fourOfAKind':
     if (/(.)\1{3}/.test(resultString)) {
       score = total();
     }
     break;
 
-    case "fullHouse":
+    case 'fullHouse':
     if (/(.)\1{2}(.)\2{1}|(.)\3{1}(.)\4{2}/.test(resultString)) {
       score = 25;
     }
     break;
 
-    case "smStraight":
+    case 'smStraight':
     if (/1.*2.*3.*4|2.*3.*4.*5|3.*4.*5.*6/.test(resultString)) {
       score = 30;
     }
     break;
 
-    case "lgStraight":
+    case 'lgStraight':
     if (/12345|23456/.test(resultString)) {
       score = 40;
     }
     break;
 
-    case "yahtzee":
+    case 'yahtzee':
     if (/(.)\1{4}/.test(resultString)) {
       score = 50;
     }
     break;
 
-    case "yahtzeeBonus":
-    // if (/(.)\1{4}/.test(resultString) || yahtzee.scored=true) {
-    //   score = 100;
-    // }
-    break;
+    // case 'yahtzeeBonus':
+    //  if (/(.)\1{4}/.test(resultString) || yahtzee.scored=true) {
+    //    score = 100;
+    //  }
+    // break;
 
-    case "chance":
+    case 'chance':
     score = total();
     break;
 
-    case "total":
+    case 'total':
     for (let elem in scoreCard) {
       if (scoreCard[elem].used === true) {
         score += scoreCard[elem].score;
       }
+    }
+    if (score > gameStats.highScore) {
+      gameStats.highScore = score;
+      $('#highScore').text('High Score: ' + gameStats.highScore);
+      localStorage.setItem('highScore', JSON.stringify(score));
     }
     break;
   }
@@ -178,26 +185,24 @@ function drawScoreCard() {
     let $tableRow = $('<tr>');
     $tableRow.addClass('category');
     let $td = $('<td>');
-    $td.text(scoreCard[elem].name);
+    if (scoreCard[elem].name === 'Bonus') {
+      $td.text('Bonus (score 35 if above categories > 62)');
+    } else {
+      $td.text(scoreCard[elem].name);
+    }
     $tableRow.append($td);
     $td = $('<td>');
-    $td.addClass("center-align");
+    $td.addClass('center-align');
     let thisRollScore = getScore(elem.toString());
 
     $td.text(thisRollScore);
 
-    if (scoreCard[elem].used === false && scoreCard[elem].section !== "none") {
+    if (scoreCard[elem].used === false && scoreCard[elem].section !== 'none') {
       $td.addClass('indigo lighten-5');
-      $tableRow.on('click', (event) => {
-        scoreCard[elem].score = thisRollScore;
-        scoreCard[elem].used = true;
-        gameStats.remainingRolls = 3;
-        drawScoreCard();
-        resetDice();
-        removeDiceListeners();
-        addRollListener();
-      });
-    } else if (scoreCard[elem].section === "none") {
+      console.log("why you stop");
+      $tableRow.addClass('clickable');
+      addRowListeners(elem, $tableRow, thisRollScore);
+    } else if (scoreCard[elem].section === 'none') {
       scoreCard[elem].score = thisRollScore;
     } else {
       $td.text(scoreCard[elem].score);
@@ -207,19 +212,63 @@ function drawScoreCard() {
   }
 }
 
+function addRowListeners(elem, $tr, thisRollScore) {
+  $tr.on('click', () => {
+    scoreCard[elem].score = thisRollScore;
+    scoreCard[elem].used = true;
+    gameStats.remainingRolls = 3;
+    updateTips();
+    drawScoreCard();
+    removeScoreCardListeners();
+    resetDice();
+    removeDiceListeners();
+    removeRollListener();
+    addRollListener();
+    checkGameOver();
+  });
+}
+
 function resetScoreCard() {
   for (const elem in scoreCard) {
     scoreCard[elem].score = 0;
     scoreCard[elem].used = false;
   }
 }
+
 function removeScoreCardListeners() {
   $('.category').off('click');
+  $('.category').removeClass('clickable');
+}
+
+function updateTips() {
+  const rolls = gameStats.remainingRolls;
+  if (rolls === 3) {
+    $('#turnTips').text('Click Roll');
+  }
+  else if (rolls === 0){
+    $('#turnTips').text('Click Category to Score');
+  } else {
+    $('#turnTips').text('Click Dice you wish to keep and roll again or Click Category to score');
+  }
+}
+
+function checkGameOver() {
+  let gameOver = true;
+  for (const elem in scoreCard) {
+    if (scoreCard[elem].used === false && scoreCard[elem].section !== 'none') {
+      gameOver = false;
+    }
+  }
+  if (gameOver) {
+    $('#controls').addClass('hide');
+    $('#finalScore').text(gameStats.playerName + "'s Final Score: " + scoreCard.total.score);
+    $('#finalScore').removeClass('hide');
+  }
 }
 // dice functions //
 function drawDice(face, space) {
   const $dice = $('<div>');
-  $dice.addClass('dice');
+  $dice.addClass('dice clickable');
   function addDot(target, number) {
     for(let j = 0; j < number; j++) {
       target.append($('<span>').addClass('dot'));
@@ -291,7 +340,6 @@ function drawDice(face, space) {
 function unlockDice() {
   for (let i = 0; i < dice.length; i++) {
     dice[i].locked = false;
-    console.log(dice[i].locked);
   }
 }
 function resetDice() {
@@ -300,9 +348,10 @@ function resetDice() {
   }
   unlockDice();
   drawAllDice();
+  $('.dice').removeClass('clickable');
   //reset roll counter
   gameStats.remainingRolls = 3;
-  $('#remainingRolls').text("Remaining Rolls: " + gameStats.remainingRolls);
+  $('#remainingRolls').text('Remaining Rolls: ' + gameStats.remainingRolls);
 }
 function rollDice() {
   for (let i = 0; i < dice.length; i++) {
@@ -310,11 +359,12 @@ function rollDice() {
       dice[i].currentValue = Math.floor(Math.random() * 6 + 1);
     }
   }
-  // unlockDice();
 }
+
 function $diceSpace(index) {
   return $('#space' + (index + 1));
 }
+
 function drawAllDice() {
   for (let i = 0; i < dice.length; i++) {
     $diceSpace(i).empty();    drawDice(dice[i].currentValue, $diceSpace(i));
@@ -338,19 +388,23 @@ function addDiceListeners() {
     });
   }
 }
+
 function removeDiceListeners() {
   for (let i = 0; i < dice.length; i ++) {
     $diceSpace(i).off('click');
+    $('.dice').removeClass('clickable');
   }
 }
 
 // listeners
 $(document).ready(function() {
+  $('#highScore').text('High Score: ' + gameStats.highScore);
   $('.modal-trigger').leanModal();
 });
 
 function addRollListener() {
   $('#roll').removeClass('grey');
+
   $('#roll').on('click', (event) => {
     removeDiceListeners();
     addDiceListeners();
@@ -358,14 +412,15 @@ function addRollListener() {
     drawAllDice();
     drawScoreCard();
     gameStats.remainingRolls --;
-    $('#remainingRolls').text("Remaining Rolls: " + gameStats.remainingRolls);
+    $('#remainingRolls').text('Remaining Rolls: ' + gameStats.remainingRolls);
     if (gameStats.remainingRolls === 0) {
       removeRollListener();
     }
+    updateTips();
   });
 }
+
 function removeRollListener() {
-  console.log("unlockDice");
   unlockDice();
   drawAllDice();
   $('#roll').off('click');
@@ -373,12 +428,20 @@ function removeRollListener() {
   removeDiceListeners();
 }
 
-$('#newGame').on('click', (event) => {
+$('#turnTips').on('click', () => {
+  $('#turnTips').toggleClass('hide');
+});
+
+$('#newGame').on('click', () => {
+  $('#finalScore').addClass('hide');
+  removeRollListener();
+  addRollListener();
   $('#controls').removeClass('hide');
   removeDiceListeners();
   resetDice();
-  resetScoreCard()
+  resetScoreCard();
   drawScoreCard();
+  removeScoreCardListeners();
   $('.playerName').text(gameStats.playerName);
-  addRollListener();
+  updateTips();
 });
